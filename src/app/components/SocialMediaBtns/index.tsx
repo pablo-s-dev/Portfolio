71bd53e309468dd151ff36ce4225c14bbcb04322
pablo-s-dev <ps.login.username@gmail.com>
2024-01-29 17:36:34 -0300

    typingEffect and imgBorder

--- src/app/components/SocialMediaBtns/index.tsx
+++ src/app/components/SocialMediaBtns/index.tsx
@@ -4,7 +4,7 @@ export default function SocialMediaBtns() {
 
     const svgIcons = Object.values({
         linkedin: (
-            <a href="https://www.linkedin.com/in/pablos-dev/" target="_blank">
+            <a href="https://www.linkedin.com/in/pablos-dev/" target="_blank" key={1}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 128 128">
                     <path fill="#0076b2" d="M116 3H12a8.91 8.91 0 0 0-9 8.8v104.42a8.91 8.91 0 0 0 9 8.78h104a8.93 8.93 0 0 0 9-8.81V11.77A8.93 8.93 0 0 0 116 3" />
                     <path fill="#fff" d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 1 1-10.5 10.49a10.5 10.5 0 0 1 10.5-10.49m20.41 29h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z" />
@@ -12,7 +12,7 @@ export default function SocialMediaBtns() {
             </a>
         ),
         Github: (
-            <a href="https://github.com/pablo-s-dev" target="_blank">
+            <a href="https://github.com/pablo-s-dev" target="_blank" key={2}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" /></svg>
             </a>
         ),
--- src/app/components/Welcome/index.tsx
+++ src/app/components/Welcome/index.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { RefObject, useRef } from 'react';
+import { RefObject, useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import Navbar from '../Navbar';
 import SocialMediaBtns from '../SocialMediaBtns';
 import styles from './welcome.module.css'
 import { MotionValue, motion, useScroll, useTransform } from "framer-motion"
+import React from 'react';
 
 export default function Welcome() {
 
@@ -73,13 +74,205 @@ function Name() {
 // }
 
 function Intro() {
+
+    type TypingTextProps = {
+        text: string,
+        start?: number | undefined,
+        timePerChar?: number | undefined;
+    }
+
+    type TextState = {
+        amount: number,
+        showCursor: boolean,
+        beginTyping: boolean
+    };
+
+    function TypingEffectBlock({ children, timePerChar }: { children: React.ReactElement[], timePerChar: number }) {
+
+        const lastIndex = useRef(0);
+        let textElementIdx = 0;
+        let lastDur = 0
+        let lastStart = 0
+        const delay = 10
+
+
+
+        const wrap = wrapStrings(
+            <>
+                {children}
+            </>, false)
+
+        textElementIdx = 0
+        lastDur = 0
+        lastStart = 0
+        lastIndex.current = 0;
+
+        const invisibleWrap = wrapStrings(
+            <>
+                {children}
+            </>, true)
+
+
+
+        lastIndex.current = textElementIdx - 1;
+
+
+        return (
+            <div style={{ display: 'grid', gridTemplateAreas: 'a' }}>
+                <div style={{ gridArea: 'a', width: '100%', textAlign: 'left', height: '100%' }}>{wrap}</div>
+
+                <div style={{ visibility: 'hidden', gridArea: 'a', width: '100%' }}>{invisibleWrap}</div>
+            </div>
+        )
+
+
+        function wrapStrings(element: React.ReactElement, skip: boolean): React.ReactNode[] {
+
+
+
+            const wrap = React.Children.map(element.props.children, (child: React.ReactElement) => {
+
+
+
+                if (typeof child === "string") {
+
+                    const curDur = (child as string).length * timePerChar + delay;
+
+                    const start = lastStart + lastDur
+
+                    console.log(textElementIdx)
+
+                    const wrap = (<TextToBeTyped text={child} start={skip ? 0 : start} timePerChar={skip ? 0 : timePerChar} index={textElementIdx} lastIdx={lastIndex} />)
+
+                    lastStart = start;
+
+
+                    lastDur = curDur
+
+                    textElementIdx += 1;
+
+                    return wrap;
+                }
+
+                if (React.isValidElement(child)) {
+                    return React.cloneElement(child, {}, wrapStrings(child, skip));
+                }
+
+                return child;
+            })
+
+            return wrap;
+        }
+
+        function TextToBeTyped({ text, timePerChar, start, index, lastIdx }: { text: string, timePerChar?: number, start?: number, index?: number, lastIdx?: React.MutableRefObject<number> }) {
+
+            const ran = useRef(false);
+
+
+            const keepCursor = useMemo(() => {
+                console.log("INDEX: ", index)
+
+                return index === lastIdx!.current
+            }, [index, lastIdx])
+
+            console.log(index)
+
+
+            const [textState, setTextState] = useState<TextState>({ amount: 0, showCursor: false, beginTyping: false });
+
+            const j = useRef(0);
+
+            useEffect(() => {
+
+                if (ran.current) return;
+
+                ran.current = true;
+
+
+                const fullText = text;
+
+
+
+
+                setTimeout(async () => {
+
+
+
+                    // show cursor
+                    setTextState(prevTextState => {
+
+
+                        const newTextState = { ...prevTextState, showCursor: true };
+
+
+
+                        return newTextState;
+
+                    });
+
+
+                    // then start typing
+                    const typingInterval = setInterval(async () => {
+
+
+
+                        if (j.current > fullText.length) {
+
+
+                            setTextState(prevTextState => {
+
+                                const newTextState = { ...prevTextState, showCursor: keepCursor ?? false };
+
+
+                                return newTextState;
+                            });
+
+
+
+
+                            clearInterval(typingInterval);
+
+
+                        }
+
+                        setTextState(prevTextState => {
+
+
+                            const newTextState = { ...prevTextState, lastIndexVisible: j.current };
+
+
+                            return newTextState;
+                        });
+
+                        j.current++;
+
+                    }, timePerChar);
+                }, start)
+
+            }, [])
+
+
+            const cursor = "|";
+
+            return (
+                <>
+                    <span>{text.slice(0, j.current)}</span><span className={styles.cursor} style={{ visibility: textState.showCursor && timePerChar != 0 ? 'visible' : 'hidden' }}>{cursor}</span>
+                </>
+
+            )
+
+        }
+    }
+
+
     return (
         <div className={styles.intro}>
-            <div>
-                <h1>Olá, me chamo <strong style={{ color: '#5924E7' }}><br />Pablo Santana.</strong> </h1>
-                <h1>Desenvolvedor <br /><strong style={{ color: '#5924E7' }}>Full Stack.</strong></h1>
-            </div>
-            {/* <Contact /> */}
+            <TypingEffectBlock timePerChar={70}>
+                <h1>Olá, me chamo</h1>
+                <h1><strong>Pablo Santana.</strong></h1>
+                <h1>Desenvolvedor</h1>
+                <h1><strong>Full Stack.</strong></h1>
+            </TypingEffectBlock>
         </div>
     )
 }
@@ -88,7 +281,10 @@ function Pic() {
 
 
     return (
-        <div className={styles.img}> </div>
+        <div className={styles.img}>
+
+        </div>
+
     )
 }
 
@@ -101,3 +297,10 @@ function NavbarButton({ children }: { children: React.ReactNode }) {
     )
 }
 
+async function waitFor(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(null);
+        }, ms)
+    })
+}
